99451337fb9083eb4c4c328f8ba9a34804f2ea7b
Taylor Maxwell <taylor@imaxwell.net>
2020-05-18 08:31:52 -0700

    Initial Check in with English Audio

---
+++ src/VideoPlayer.js
@@ -0,0 +1,110 @@
+/*
+If you are using server-side rendering, remember that this component should be loaded on client-side
+shaka player needs to be loaded on client-side, loading it on server-side may lead to error or undesired results
+*/
+
+/*
+importing dependencies and CSS file(s) required for UI customization
+https://github.com/amit08255/shaka-player-react-with-ui-config/tree/master/with-ui-configuration
+*/
+import React from "react";
+import "shaka-player/dist/controls.css";
+const shaka = require("shaka-player/dist/shaka-player.ui.js");
+
+//Creating class component
+class VideoPlayer extends React.PureComponent {
+  constructor(props) {
+    super(props);
+
+    //Creating reference to store video component on DOM
+    this.videoComponent = React.createRef();
+
+    //Creating reference to store video container on DOM
+    this.videoContainer = React.createRef();
+
+    //Initializing reference to error handlers
+    this.onErrorEvent = this.onErrorEvent.bind(this);
+    this.onError = this.onError.bind(this);
+  }
+
+  onErrorEvent(event) {
+    // Extract the shaka.util.Error object from the event.
+    this.onError(event.detail);
+  }
+
+  onError(error) {
+    // Log the error.
+    console.error("Error code", error.code, "object", error);
+  }
+
+  componentDidMount() {
+    //Link to MPEG-DASH video
+    const manifestUri =
+      "https://storage.googleapis.com/shaka-demo-assets/angel-one/dash.mpd";
+
+    //Getting reference to video and video container on DOM
+    const video = this.videoComponent.current;
+    const videoContainer = this.videoContainer.current;
+
+    //Initialize shaka player
+    let player = new shaka.Player(video);
+
+		let config = player.getConfiguration();
+		console.log(config);
+
+    //Setting UI configuration JSON object
+    const uiConfig = {};
+
+    //Configuring elements to be displayed on video player control panel
+    uiConfig["controlPanelElements"] = [
+      "mute",
+      "volume",
+      "time_and_duration",
+      "fullscreen",
+			"overflow_menu",
+    ];
+
+    //Setting up shaka player UI
+    const ui = new shaka.ui.Overlay(player, videoContainer, video);
+
+    ui.configure(uiConfig); //configure UI
+    ui.getControls();
+
+		// https://shaka-player-demo.appspot.com/docs/api/tutorial-config.html
+		// https://shaka-player-demo.appspot.com/docs/api/shaka.extern.html#.PlayerConfiguration
+		player.configure({
+			preferredAudioLanguage: 'en-us'
+		})
+    // Listen for error events.
+    player.addEventListener("error", this.onErrorEvent);
+
+    // Try to load a manifest.
+    // This is an asynchronous process.
+    player
+      .load(manifestUri)
+      .then(function () {
+        // This runs if the asynchronous load is successful.
+        console.log("The video has now been loaded!");
+      })
+      .catch(this.onError); // onError is executed if the asynchronous load fails.
+  }
+
+  render() {
+    /*
+		Returning video with a container. Remember, when setting up shaka player with custom UI, you must
+		add your video component inside a container
+		The container will be used by shaka player to add your customized UI for the player
+		*/
+    return (
+      <div className="video-container" ref={this.videoContainer}>
+        <video
+          className="shaka-video"
+          ref={this.videoComponent}
+          poster="//shaka-player-demo.appspot.com/assets/poster.jpg"
+        />
+      </div>
+    );
+  }
+}
+
+export default VideoPlayer;
